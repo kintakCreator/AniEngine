d82bfb459aeb5821da36d4c30c724b4260a0dfeb
unknown <kinas5727@gmail.com>
2020-08-30 10:34:42 +0300

    Added Shader Loader class

---
+++ src/Renderer/ShaderProgram.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+namespace Renderer
+{
+
+	class ShaderProgram
+	{
+	public:
+		ShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+		~ShaderProgram();
+		bool isCompiled() const { return m_isCompiled; }
+		void use() const;
+		
+		// hack protection
+		ShaderProgram() = delete;
+		ShaderProgram(ShaderProgram&) = delete; // shader program from other shader program... really?
+		ShaderProgram& operator=(const ShaderProgram&) = delete;
+		ShaderProgram& operator=(ShaderProgram&& shaderProgram) noexcept;
+		ShaderProgram(ShaderProgram&& shaderProgram) noexcept;
+
+	private:
+		bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
+		bool m_isCompiled = false;
+		GLuint m_ID = 0;
+	};
+
+}
