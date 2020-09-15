c7751730ed7edce635602531902ced35723e9cdd
unknown <kinas5727@gmail.com>
2020-09-15 07:25:01 +0300

    Added Texture2D class

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,5 +1,6 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 
 #include <sstream>
 #include <fstream>
@@ -70,7 +71,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const
 	return nullptr;
 }
 
-void ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
 {
 	int channels = 0;
 	int width = 0;
@@ -84,11 +85,31 @@ void ResourceManager::loadTexture(const std::string& textureName, const std::str
 	if (!pixels)
 	{
 		std::cerr << "Can't load texture: " << texturePath << std::endl;
-		return;
+		return nullptr;
 	}
 
 	std::cout << textureName << " has been loaded!" << std::endl;
 
+	std::shared_ptr<Renderer::Texture2D> newTexture = 
+		m_textures.emplace(textureName,
+		std::make_shared<Renderer::Texture2D>(width, height, 
+											  pixels, channels, 
+											  GL_NEAREST, GL_CLAMP_TO_EDGE)).first->second;
+
 	/* memory leak fix */
 	stbi_image_free(pixels);
+
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName)
+{
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end())
+	{
+		return it->second;
+	}
+
+	std::cerr << "Can't find the texture: " << textureName << std::endl;
+	return nullptr;
 }
